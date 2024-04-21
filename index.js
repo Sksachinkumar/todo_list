1a221bae43359624f902576fa79f46cd4e9cb0cb
Sachin <160403516+Sksachinkumar@users.noreply.github.com>
2024-04-21 18:29:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let conditions = true;
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todo',
+            type: 'input',
+            message: "what you want to add you Todo ?"
+        },
+        {
+            name: 'addMore',
+            type: 'confirm',
+            message: "what  you want to add more ?",
+            default: 'false'
+        }
+    ]);
+    console.log(addTask.todo);
+    conditions = addTask.addMore;
+    todos.push(addTask.todo);
+}
